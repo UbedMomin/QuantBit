fc6da5b589cd5d8669ad83a893afe4544a11d8f5
maheshmohite96 <maheshmohite1093@gmail.com>
2025-11-13 13:39:52 +0530

    some changes of the env and server

--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import requestRoutes from "./routes/requestRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -17,19 +18,19 @@ const app = express();
 app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
 app.use(express.json());
 
-// Serve uploaded images (if using local uploads)
+// Serve uploaded images (if local)
 app.use("/uploads", express.static("uploads"));
 
 // ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/request", requestRoutes);
 app.use("/api/report", reportRoutes);
+app.use("/api/upload", uploadRoutes);
 
 // ROOT ROUTE
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
 
-// SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
